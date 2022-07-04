0ce4c376154f89f13f18c45c8c163279176b80df
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-07-04 22:25:16 +0530

    Added:Calculate till max days/hours reaches

---
+++ CalculateTillMaxDaysReaches.java
@@ -0,0 +1,34 @@
+package com.BridgeLabz.empWage;
+
+public class CalculateTillMaxDaysReaches {
+	
+	public static void main(String[] args) {
+
+		int empRatePerHour = 20;
+		int salary;
+		int empHrs;
+		int totalSalary = 0;
+		int totalWorkingdays = 0,totalEmpHrs = 0; 
+	
+		while(totalEmpHrs <= 100 && totalWorkingdays <=20)
+		{
+		double empCheck = Math.floor(Math.random()*10)%3;
+		switch((int)empCheck)
+			{
+			case 1:
+			empHrs = 8;
+			break;
+			case 2: 
+			empHrs = 4;
+			break;
+			default:
+			empHrs =0;
+			}
+		salary = empRatePerHour*empHrs;
+		totalSalary = totalSalary + salary;
+		totalEmpHrs = totalEmpHrs + empHrs;
+		totalWorkingdays++;
+		}
+		System.out.println("Monthly wage is " + totalSalary);
+}
+}
